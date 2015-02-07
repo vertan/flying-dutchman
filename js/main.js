fe91b93401bc8b803dfe72c4e993067e6f24db08
Andreas Wennerberg <AndreasSE93@gmail.com>
2015-02-07 15:23:21 +0100

    Added error message box and moved the script tag back to the head

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-var pinField = document.getElementById("pinField");
-
 function insertSelf(e) {
 	console.log(e);
 	pinField.value += e.target.innerHTML;
@@ -8,9 +6,15 @@ function backspace(e) {
 	pinField.value = pinField.value.substr(0, pinField.value.length - 1);
 }
 
-var pinButtons = document.getElementsByClassName("pin-digit");
-console.log(pinButtons, pinButtons.length);
-for (var el of pinButtons) {
-	el.addEventListener("click", insertSelf, false);
+function init() {
+	var pinField = document.getElementById("pinField");
+	var pinButtons = document.getElementsByClassName("pin-digit");
+	console.log(pinButtons, pinButtons.length);
+	for (var el of pinButtons) {
+		el.addEventListener("click", insertSelf, false);
+	}
+	document.getElementById("eraseButton").addEventListener("click", backspace, false);
 }
-document.getElementById("eraseButton").addEventListener("click", backspace, false);
+
+window.addEventListener("load", init, false);
+
