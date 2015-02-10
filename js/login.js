25b1a08f2785ac917638e311e29780f4f00e8369
Andreas Wennerberg <AndreasSE93@gmail.com>
2015-02-10 21:05:17 +0100

    Structure

--- js/login.js
+++ js/login.js
@@ -1,3 +1,6 @@
+var pinField;
+var pinButtons;
+
 sessionStorage.clear();
 
 function login(e) {
@@ -8,9 +11,24 @@ function login(e) {
 	location.assign("main.html");
 }
 
-function initLogin() {
+function insertSelf(e) {
+	console.log(e);
+	pinField.value += e.target.innerHTML;
+}
+function backspace(e) {
+	pinField.value = pinField.value.substr(0, pinField.value.length - 1);
+}
+
+function init() {
+	pinField = document.getElementById("pinField");
+	pinButtons = document.getElementsByClassName("pin-digit");
+	console.log(pinButtons, pinButtons.length);
+	for (var i = 0; i < pinButtons.length; i++) {
+		pinButtons[i].addEventListener("click", insertSelf, false);
+	}
+	document.getElementById("eraseButton").addEventListener("click", backspace, false);
 	document.getElementById("sideArea").addEventListener("submit", login, false);
 }
 
-window.addEventListener("load", initLogin, false);
+window.addEventListener("load", init, false);
 
