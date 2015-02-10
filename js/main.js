d3eedf841e44d2e39d79730dad31a750bca8dde7
Andreas Wennerberg <AndreasSE93@gmail.com>
2015-02-10 15:50:04 +0100

    Fixed keypad in non-Firefox-like browsers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var pinField;
+var pinButtons;
+
 function insertSelf(e) {
 	console.log(e);
 	pinField.value += e.target.innerHTML;
@@ -7,11 +10,11 @@ function backspace(e) {
 }
 
 function init() {
-	var pinField = document.getElementById("pinField");
-	var pinButtons = document.getElementsByClassName("pin-digit");
+	pinField = document.getElementById("pinField");
+	pinButtons = document.getElementsByClassName("pin-digit");
 	console.log(pinButtons, pinButtons.length);
-	for (var el of pinButtons) {
-		el.addEventListener("click", insertSelf, false);
+	for (var i = 0; i <= pinButtons.length; i++) {
+		pinButtons[i].addEventListener("click", insertSelf, false);
 	}
 	document.getElementById("eraseButton").addEventListener("click", backspace, false);
 }
