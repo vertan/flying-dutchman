5a622febfb9cce47cadc84178389ad4e489c3da0
Andreas Wennerberg <AndreasSE93@gmail.com>
2015-03-01 15:19:00 +0100

    Added manager login

--- js/login-username.js
+++ js/login-username.js
@@ -1,5 +1,8 @@
 var usernameField;
 var passwordField;
+var adminLoginButton;
+
+var isAdmin;
 
 var db;
 
@@ -10,8 +13,7 @@ function login(response) {
 		sessionStorage.setItem("userID",    response.payload[0].user_id);
 		sessionStorage.setItem("firstName", response.payload[0].first_name);
 		sessionStorage.setItem("lastName",  response.payload[0].last_name);
-		alert("Welcome " + response.payload[0].first_name + " " + response.payload[0].last_name + ".");
-		location.assign("main.html");
+		location.assign(isAdmin ? "admin.html" : "main.html");
 	} else if (response.type == "error") {
 		var errorMsg = response.payload[0].msg;
 		alert("Error:\n" + errorMsg);
@@ -29,10 +31,22 @@ function initLogin(e) {
 	db.request("iou_get", login);
 }
 
+function initAdminLogin(e) {
+	isAdmin = true;
+	initLogin(e);
+}
+
+function initUserLogin(e) {
+	isAdmin = false;
+	initLogin(e);
+}
+
 function init() {
 	usernameField = document.getElementById("username");
 	passwordField = document.getElementById("password");
-	document.getElementById("loginForm").addEventListener("submit", initLogin, false);
+	adminLoginButton = document.getElementById("adminLoginButton");
+	document.getElementById("loginForm").addEventListener("submit", initUserLogin, false);
+	document.getElementById("adminLoginButton").addEventListener("click", initAdminLogin, false);
 }
 
 window.addEventListener("load", init, false);
