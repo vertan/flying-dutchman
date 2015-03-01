933ca4114afc7875c4994ad3ab586e2ca65490d9
Andreas Wennerberg <AndreasSE93@gmail.com>
2015-03-01 16:21:44 +0100

    Added full names to users list.

--- js/userList.js
+++ js/userList.js
@@ -4,13 +4,13 @@ var userListElement;
 function userList(response) {
 	if (response.type == "iou_get_all") {
 		var list = document.getElementById("List");
-		for(var i=0; i<response.payload.length; i++) {
-			var username = response.payload[i].username;
+		for(var i = 0; i<response.payload.length; i++) {
+			var user = response.payload[i];
 			var option = document.createElement("option");
-			option.innerHTML = username;
+			option.value = user.username;
+			option.innerHTML = user.first_name + " " + user.last_name + " (" + user.username + ")";
 			list.appendChild(option);
 		}
-			
 	} else if (response.type == "error") {
 		var errorMsg = response.payload[0].msg;
 		alert("Error:\n" + errorMsg);
@@ -24,7 +24,6 @@ function init() {
 	var password = sessionStorage.getItem("password");
 	db = new Database(username, password);
 	db.request("iou_get_all", userList);
-	alert(username);
 }
 
 window.addEventListener("load", init, false);
