d1f37c59ca1fcd2ca83973df8b0438a4d0ed3c8c
sverrirth <sverrir@gmx.com>
2015-03-04 11:56:46 +0100

    Designed shopping cart
    Work in progress

--- js/main.js
+++ js/main.js
@@ -6,4 +6,23 @@ function init() {
 	document.getElementById("credit").innerHTML = S("credit", credit + " kr");
 }
 
+$(function() {
+	$('#cart').droppable({
+		tolerance: 'pointer',
+		drag: function(event, ui) {
+			
+		}
+	});
+	$('#favorites button').draggable({
+		cancel: false,
+		helper: "clone",
+		revert: true,
+		drag: function(event, ui) {
+			console.log(event);
+			console.log(ui);
+			$(ui.helper).addClass("nicer");
+		}
+	});
+});
+
 window.addEventListener("DOMContentLoaded", init, false);
