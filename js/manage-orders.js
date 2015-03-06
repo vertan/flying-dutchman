e3725ff71a9c66fda9041d6e75a31b13a2371c2f
Andreas Wennerberg <AndreasSE93@gmail.com>
2015-03-06 14:23:19 +0100

    Cleaned up orders list cde and switched to the Database object instead of JQuery.

--- js/manage-orders.js
+++ js/manage-orders.js
@@ -1,20 +1,23 @@
-$(document).ready(function(){
-	$.ajax({
-        url: 'http://pub.jamaica-inn.net/fpdb/api.php?username=jorass&password=jorass&action=inventory_get',
-        dataType: "json",
-        async: true,
-        success: function (result) {
+function listInventory(result) {
+	for (var i = 0; i < result.payload.length; i++) {
+		var row = document.createElement("tr");
+		row.innerHTML =
+			"<td>" + result.payload[i].beer_id   + "</td>" +
+			"<td>" + result.payload[i].namn      + "</td>" +
+			"<td>" + result.payload[i].namn2     + "</td>" +
+			"<td>" + result.payload[i].sbl_price + "</td>" +
+			"<td>" + result.payload[i].pub_price + "</td>" +
+			"<td>" + result.payload[i].price     + "</td>" +
+			"<td>" + result.payload[i].count     + "</td>";
+		document.getElementById("manage-orders").appendChild(row);
+	}
+}
 
-			for(var i = 0; i < result.payload.length; i++) {
-				
-				$('#manage-orders').append('<tr><td>'+result.payload[i].beer_id +'</td><td>'+result.payload[i].namn +'</td><td>'+result.payload[i].namn2 +'</td><td>'+result.payload[i].sbl_price +'</td><td>'+result.payload[i].pub_price +'</td><td>'+result.payload[i].price+'</td><td>'+result.payload[i].count +'</td></tr>')
-				
-				
-			}
-			},
-        error: function (request,error) {
-		
-            alert('Network error has occurred please try again!');
-        }
-    }); 
-});
+function initOrderList() {
+	var username = sessionStorage.getItem("username");
+	var password = sessionStorage.getItem("password");
+	var db = new Database(username, password);
+	db.request("inventory_get", listInventory);
+}
+
+document.addEventListener("DOMContentLoaded", initOrderList, false);
