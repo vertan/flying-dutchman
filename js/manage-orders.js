018a2a29b2b0e17accd4629c666f02c0cb06ff06
JohannaLofvenberg <johannalofvenberg@hotmail.com>
2015-03-06 15:24:11 +0100

    manage-orders.js
    manage-orders.js only lists beers that are or almost are out of stock
    now, and they're sorted.

--- js/manage-orders.js
+++ js/manage-orders.js
@@ -1,14 +1,21 @@
 function listInventory(result) {
+	result.payload.sort(function(a, b) {
+		if (a.count-0 < b.count-0) return -1;
+		if (a.count-0 > b.count-0) return  1;
+		else                   return  0;
+		});
 	for (var i = 0; i < result.payload.length; i++) {
 		var row = document.createElement("tr");
-		row.innerHTML =
-			"<td>" + result.payload[i].beer_id   + "</td>" +
-			"<td>" + result.payload[i].namn      + "</td>" +
-			"<td>" + result.payload[i].namn2     + "</td>" +
-			"<td>" + result.payload[i].sbl_price + "</td>" +
-			"<td>" + result.payload[i].pub_price + "</td>" +
-			"<td>" + result.payload[i].price     + "</td>" +
-			"<td>" + result.payload[i].count     + "</td>";
+		if (result.payload[i].count < 30) {
+			row.innerHTML =
+				"<td>" + result.payload[i].beer_id   + "</td>" +
+				"<td>" + result.payload[i].namn      + "</td>" +
+				"<td>" + result.payload[i].namn2     + "</td>" +
+				"<td>" + result.payload[i].sbl_price + "</td>" +
+				"<td>" + result.payload[i].pub_price + "</td>" +
+				"<td>" + result.payload[i].price     + "</td>" +
+				"<td>" + result.payload[i].count     + "</td>";
+		}	
 		document.getElementById("manage-orders").appendChild(row);
 	}
 }
