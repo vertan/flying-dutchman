6528f6361679a3702775d101f666484b5ca5f765
Filip Hedman <hedman.filip@gmail.com>
2015-03-15 14:04:45 +0100

    Fade on scroll

--- js/main.js
+++ js/main.js
@@ -17,3 +17,11 @@ $(function() {
 		}
 	});
 });
+
+$(document).scroll(function() {
+	if($(document).scrollTop() >= 20) {
+		$('#main-header').css('background', 'rgba(0, 0, 0, 0.9)');
+	} else {
+		$('#main-header').css('background', 'rgba(0, 0, 0, 0.45)');
+	}
+});
