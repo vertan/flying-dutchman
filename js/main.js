56f9b6f1f7d0a1f476e26486d6afa7369af572ff
sverrirth <sverrir@gmx.com>
2015-03-15 14:54:15 +0100

    cart works properly + we now have undo functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
+var beersPicked = [];
+
 $(function() {
 	$('#cart').droppable({
 		tolerance: 'pointer',
 		drop: function(event, ui) {
 			var name = ui.helper[0].getElementsByClassName("name")[0].innerHTML;
-			console.log(name);
 			var newElement = document.createElement("div");
 			newElement.innerHTML = name;
 			document.getElementById("cart").appendChild(newElement);
+			var ID = ui.helper[0].dataset.beerid;
+			newElement.id = "incart" + ID;
+			beersPicked.push(ID);
+			console.log(beersPicked);
 		}
 	});
 	$('#favorites button').draggable({
@@ -18,3 +23,11 @@ $(function() {
 		}
 	});
 });
+
+function undo() {
+	var removeID = "incart" + beersPicked[beersPicked.length-1];
+	console.log(document.getElementById(removeID));
+	console.log(document.getElementById("cart"));
+	document.getElementById("cart").removeChild(document.getElementById(removeID));
+	beersPicked.length--;
+}
