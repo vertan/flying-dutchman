1f9fdc539d3311c0016e2dac7ed558ffe238ac66
lundgrenjohan <johan.lundgren86@gmail.com>
2015-03-15 15:46:53 +0100

    all stats pages
    * stats_beer_year.html:

--- js/stats.js
+++ js/stats.js
@@ -1,30 +1,34 @@
 window.addEventListener("load", init, false);
 
-/*
-var customers = ["Johan", "Filip", "Johanna", "Andreas"];
-var c_values = [16, 8, 4, 3];
-*/
-var c_customers = [
+var c_customers_day = [
 		{ "name" : "Johan", "visits" : 16 },
 		{ "name" : "Filip", "visits" : 8 },
 		{ "name" : "Johanna", "visits" : 4 },
 		{ "name" : "Andreas", "visits" : 3 }
 ];
 
+var c_customers_week = [
+		{ "name" : "Johan", "visits" : 34 },
+		{ "name" : "Filip", "visits" : 23 },
+		{ "name" : "Johanna", "visits" : 16 },
+		{ "name" : "Andreas", "visits" : 13 }
+];
 
-// var c_color = ["#004CB3", "#B3004C", "#4CB300"];
+var c_customers_year = [
+		{ "name" : "Johan", "visits" : 324 },
+		{ "name" : "Filip", "visits" : 235 },
+		{ "name" : "Johanna", "visits" : 106 },
+		{ "name" : "Andreas", "visits" : 133 }
+];
+
 var c_days = ["Mon", "Tue", "Wes", "Thu", "Fri", "Sat", "Sun"];
 var c_month = ["Jan", "Feb", "Mars", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-
-var c_beer_attr = [ 
-		{ "name" : "Kungen", "price" : 220},
-		{ "name" : "Mariestad", "price" : 281},
-		{ "name" : "Åbro", "price" : 343} //,
-//		{ "name" : "Spendrups", "price" : 349}
+var c_beer_day = [
+		{ "name" : "Kungen", "sales" : [10] },
+		{ "name" : "Mariestad", "sales" : [16] },
+		{ "name" : "Åbro", "sales" : [13] }
 ];
-
-var c_beer_day = [ { "name" : "", "sales" : [] }, { "name" : "", "sales" : [] } ];
 var c_beer_week = [
 		{ "name" : "Kungen", "sales" : [ 23, 34, 32, 34, 43, 23, 26] },
 		{ "name" : "Mariestad", "sales" : [ 33, 23, 25, 12, 33, 28, 22] },
@@ -48,31 +52,26 @@ var c_beer_year = [ {
 
 
 var c_colors = [{
-		"fillStyle" : "rgba(200,100,100,0.5)",
-		"strokeStyle" :  "rgba(200,100,100,0.8)",
+		"fillStyle" : "rgba(200,100,100,1)",
 		"highlightFill" : "rgba(200,100,100,0.75)",
-		"highlightStroke" : "rgba(200,100,100,1)"
+
 },{
-		"fillStyle" :  "rgba(151,187,205,0.5)",
-		"strokeStyle" :  "rgba(151,187,205,0.8)",
+		"fillStyle" :  "rgba(151,187,205,1)",
 		"highlightFill" : "rgba(151,187,205,0.75)",
-		"highlightStroke" : "rgba(151,187,205,1)"
+
 },{
-		"fillStyle" :  "rgba(101,205,105,0.5)",
-		"strokeStyle" :  "rgba(101,205,105,0.8)",
+		"fillStyle" :  "rgba(101,205,105,1)",
 		"highlightFill" : "rgba(101,205,105,0.75)",
-		"highlightStroke" : "rgba(101,205,105,1)"
+
 },{
-		"fillStyle" :  "rgba(100,205,205,0.5)",
-		"strokeStyle" :  "rgba(100,205,205,0.8)",
+		"fillStyle" :  "rgba(100,205,205,1)",
 		"highlightFill" : "rgba(100,205,205,0.75)",
-		"highlightStroke" : "rgba(100,205,205,1)"	
 }];
 
 function getMaxValue (array) {
 
 		n = array.length;
-
+		
 		var maxValue = array[0].sales[0];
 		
 		for ( var i = 0; i < n; i++ ) {
@@ -95,8 +94,6 @@ function drawLines (canvas, xPadding, yPadding) {
 }
 
 function lineChart (ctx, array1, array2) {
-
-		// console.log(array1[1]);
 		
 		var xPadding = 30;
 		var yPadding = 30;
@@ -109,7 +106,7 @@ function lineChart (ctx, array1, array2) {
 				value = 35 + ctx.width/n * i;
 				ctx.fillText( array2[i], value, ctx.height - yPadding + 20);
 		}
-
+		
 		ctx.textAlign = "end";
 
 		maxValue = getMaxValue(array1);
@@ -123,14 +120,14 @@ function lineChart (ctx, array1, array2) {
 		}
 
 		for ( var i = 0; i < array1.length; i++) {
-				ctx.fillStyle = c_colors[i].highlightStroke;
+				ctx.fillStyle = c_colors[i].fillStyle;
 				ctx.font = "bold 15px Arial";
 				ctx.fillText(array1[i].name, ctx.width - 100 * i, ctx.height - 2 * yPadding);				
 		}
 		
 		for ( var j = 0; j < array1.length; j++) {
 
-				ctx.strokeStyle = c_colors[j].highlightStroke;
+				ctx.strokeStyle = c_colors[j].fillStyle;
 				ctx.beginPath();
 				ctx.moveTo(45, ctx.height - array1[j].sales[0]*h-yPadding);
 				
@@ -143,17 +140,15 @@ function lineChart (ctx, array1, array2) {
 				
 				for ( var i = 0; i < array1[j].sales.length; i++) {
 						value = 45 + ctx.width/n * i;
-						ctx.fillStyle = c_colors[j].highlightStroke;
+						ctx.fillStyle = c_colors[j].fillStyle;
 						ctx.beginPath();
 						ctx.arc( value, ctx.height - array1[j].sales[i]*h - yPadding, 4, 0, Math.PI * 2, true);
 						ctx.fill();
 				}
 		}
-
-
 }
 
-function barChart(ctx, n) {
+function barChart(ctx, array) {
 
 		var bar_width = 50;
 		var x = 10
@@ -161,28 +156,37 @@ function barChart(ctx, n) {
 		var xPadding = 30;
 		var yPadding = 30;
 		
+		maxValue = getMaxValue(array);
+		
+		console.log("maxValue " + maxValue);
+
+		var len = Math.ceil(Math.log(maxValue + 1) / Math.LN10);
+		roundUp = Math.ceil(maxValue/(Math.pow(10,len-1)));
+		h = ctx.height / ( roundUp *Math.pow(10, len-1));
+		
+		for ( var i = 0; i < 11; i++) {
+				value = roundUp * i * Math.pow(10, len-2);
+				console.log(value);
+				ctx.fillText(Math.ceil(value), 10, ctx.height - value * h - yPadding);
+		}
+		
+		n = array.length;
+		
 		for ( var i = 0; i < n; i++) {
 				ctx.lineWidth = 2;
 				ctx.beginPath();
 
-				ctx.rect(x + 2 * xPadding, ctx.height - c_beer_attr[i].price - yPadding, bar_width, c_beer_attr[i].price);
-				ctx.fillStyle = c_colors[0].highlightFill;
+				ctx.rect( x + 2 * xPadding, ctx.height - array[i].sales * h - yPadding, bar_width, array[i].sales*h);
+
+				ctx.fillStyle = c_colors[i].fillStyle;
 				ctx.fill();
 				ctx.strokeStyle = c_colors[0].highlightStroke;
-				ctx.stroke();
-
-				ctx.beginPath();
-				ctx.rect(x + 2 * xPadding + bar_width, ctx.height - c_beer_attr[i].price -yPadding, bar_width, c_beer_attr[i].price);
-
-				ctx.fillStyle = c_colors[1].highlightFill;
-				ctx.fill();
-				ctx.strokeStyle = c_colors[1].highlightStroke;
 				ctx.stroke();
 				
 				ctx.fillStyle = "#000";
 				ctx.font = "16px Arial";
 				ctx.textAlign = "center";
-				ctx.fillText(c_beer_attr[i].name, x + 3.5 * xPadding, ctx.height - yPadding + 20);
+				ctx.fillText(array[i].name, x + 3 * xPadding, ctx.height - yPadding + 20);
 				
 				x += bar_width * 3;
 				
@@ -213,7 +217,7 @@ function pieChart(ctx, radius, x, y, array) {
 				ctx.arc(x, y, radius, startAngle, endAngle);
 				ctx.lineTo(x, y);
 
-				ctx.fillStyle = c_colors[i].highlightStroke;
+				ctx.fillStyle = c_colors[i].fillStyle;
 
 				ctx.lineWidth = 5;
 				ctx.strokeStyle = '#ffffff';
@@ -223,7 +227,7 @@ function pieChart(ctx, radius, x, y, array) {
 
 				startAngle = endAngle;
 
-				ctx.fillStyle = c_colors[i].highlightStroke;
+				ctx.fillStyle = c_colors[i].fillStyle;
 				ctx.font = "bold 15px Arial";
 				ctx.fillText(array[i].name, 100, 100 + i * 30);
 				ctx.fillText(array[i].visits, 200, 100 + i * 30);
@@ -247,21 +251,22 @@ function init() {
 
 		console.log(chart.id);
 		switch(chart.id) {
-		case "beer_year_chart":
-				lineChart(ctx, c_beer_year, c_month);
+		case "beer_day_chart":
+				barChart(ctx, c_beer_day);
 				break;
 		case "beer_week_chart":
 				lineChart(ctx, c_beer_week, c_days);
 				break;
-		case "user_day_chart":
-				pieChart(ctx, radius, x, y, c_customers);
+		case "beer_year_chart":
+				lineChart(ctx, c_beer_year, c_month);
 				break;
+		case "user_day_chart":
+				pieChart(ctx, radius, x, y, c_customers_day);
+				break;
+		case "user_week_chart":
+				pieChart(ctx, radius, x, y, c_customers_week);
+				break;
+		case "user_year_chart":
+				pieChart(ctx, radius, x, y, c_customers_year);
 		}
-		
-		// barChart(ctx, 3);
-				// pieChart(ctx, radius, x, y, c_customers);
-
-
-		// barChart(ctx1);
-
 }
