9ae11b423034123dd7f11b7dd0a16c6c812af2eb
Andreas Wennerberg <AndreasSE93@gmail.com>
2015-03-16 17:39:25 +0100

    Reimplemented cart and undo/redo.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,105 @@
-var beersPicked = [];
-var redo = [];
+var undoStack = [];
+var redoStack = [];
+var cart = [];
+
+// http://stackoverflow.com/a/25921504
+function deepCopy(o) {
+	var out = Array.isArray(o) ? [] : {};
+	for (var key in o) {
+		var v = o[key];
+		out[key] = (typeof v === "object") ? deepCopy(v) : v;
+	}
+	return out;
+}
+
+function itemIndexOf(id) {
+	for (var i = 0; i < cart.length; i++) {
+		if (cart[i].id == id) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+function addItem(id, name, price, quantity = 1) {
+	if (itemIndexOf(id) == -1) {
+		pushUndo();
+		cart.push({
+			id: id,
+			name: name,
+			price: price,
+			quantity: quantity
+		});
+		updateCart();
+	} else {
+		changeQuantity(id, quantity);
+	}
+}
+
+function removeItem(id) {
+	var i = itemIndexOf(id);
+	if (i == -1) {
+		return 0;
+	} else {
+		pushUndo();
+		var deleted = cart.splice(i, 1);
+		updateCart();
+		return deleted[0].quantity;
+	}
+}
+
+function clearCart() {
+	pushUndo();
+	cart = [];
+	updateCart();
+}
+
+function getQuantity(id) {
+	var i = itemIndexOf(id);
+	if (i == -1) {
+		return 0;
+	} else {
+		return cart[i].quantity;
+	}
+}
+
+function setQuantity(id, newQuantity) {
+	var i = itemIndexOf(id);
+	if (i == -1) {
+		console.warn("id " + id + " not in cart");
+		return false;
+	} else {
+		pushUndo();
+		cart[i].quantity = newQuantity;
+		updateCart();
+		return true;
+	}
+}
+
+function changeQuantity(id, delta) {
+	var quantity = getQuantity(id) + delta;
+	if (quantity > 0) {
+		setQuantity(id,  quantity);
+	} else {
+		removeItem(id);
+	}
+}
+
+function pushUndo() {
+	document.getElementById("nav-undo").disabled = true;
+	redoStack = [];
+	undoStack.push(deepCopy(cart));
+	document.getElementById("nav-undo").disabled = false;
+}
 
 $(function() {
 	$('#table_order').droppable({
 		tolerance: 'pointer',
 		drop: function(event, ui) {
 			var name = ui.helper[0].getElementsByClassName("name")[0].innerHTML;
+			var price = ui.helper[0].getElementsByClassName("price")[0].innerHTML.split(" ")[0] - 0;
 			var ID = ui.helper[0].dataset.beerid;
-			beersPicked.push(ID);
-			console.log(beersPicked);
-			document.getElementById("nav-undo").disabled = false;
-			document.getElementById("nav-redo").disabled = true;
-			redo = [];
-			updateCart();
+			addItem(ID, name, price, 1);
 		}
 	});
 	$('#favorites button').draggable({
@@ -26,9 +113,13 @@ $(function() {
 });
 
 function undo() {
-	var thisID = beersPicked.pop();
-	redo.push(thisID);
-	if (beersPicked.length == 0) {
+	if (undoStack.length == 0) {
+		console.error("Undo stack empty");
+		return false;
+	}
+	redoStack.push(deepCopy(cart));
+	cart = undoStack.pop();
+	if (undoStack.length == 0) {
 		document.getElementById("nav-undo").disabled = true;
 	}
 	document.getElementById("nav-redo").disabled = false;
@@ -36,74 +127,39 @@ function undo() {
 }
 
 function redo() {
-	var thisID = redo.pop();
-	var name = document.getElementById("fav" + thisID).getElementsByClassName("name")[0].innerHTML;
-	beersPicked.push(thisID);
+	if (redoStack.length == 0) {
+		console.error("Redo stack empty");
+		return false;
+	}
+	undoStack.push(deepCopy(cart));
+	cart = redoStack.pop();
 	document.getElementById("nav-undo").disabled = false;
-	if (redo.length == 0) {
+	if (redoStack.length == 0) {
 		document.getElementById("nav-redo").disabled = true;
 	}
 	updateCart();
 }
 
-function changeContent(delta, id) {
-	if (delta < 0) {
-		for (var i = beersPicked.length - 1; i >= 0; i--) {
-			if (beersPicked[i] == id) {
-				beersPicked.splice(i, 1);
-				delta++;
-				if (delta >= 0) {
-					break;
-				}
-			}
-		}
-	} else if (delta > 0) {
-		while (delta > 0) {
-			beersPicked.push(id);
-			delta--;
-		}
-	}
-	updateCart();
-}
-
 function updateCart() {
-	var cart = document.getElementById("cart-body");
-	var items = [];
+	var tbody = document.getElementById("cart-body");
 	var sum = 0;
-	sessionStorage.setItem("cart", JSON.stringify(beersPicked));
-	sessionStorage.setItem("redo", JSON.stringify(redo));
-	for (var i = 0; i < beersPicked.length; i++) {
-		var found = false;
-		for (var j = 0; j < items.length; j++) {
-			if (items[j].id == beersPicked[i]) {
-				items[j].count++;
-				found = true;
-				break;
-			}
-		}
-		if (!found) {
-			items.push({
-				id:    beersPicked[i],
-				name:  "Beer #" + beersPicked[i],
-				price: 25,
-				count: 1
-			});
-		}
+	sessionStorage.setItem("cart", JSON.stringify(cart));
+	sessionStorage.setItem("undo", JSON.stringify(undoStack));
+	sessionStorage.setItem("redo", JSON.stringify(redoStack));
+	while (tbody.hasChildNodes()) {
+		tbody.removeChild(tbody.firstChild);
 	}
-	while (cart.hasChildNodes()) {
-		cart.removeChild(cart.firstChild);
-	}
-	for (var k = 0; k < items.length; k++) {
+	for (var i = 0; i < cart.length; i++) {
 		var row = document.createElement("tr");
 		row.innerHTML =
-			"<th class='beer-name'>" + items[k].name + "</td>" +
-			"<td class='dec'><button type='button' onclick='changeContent(-1, " + items[k].id + ")'>&minus;</button></td>" +
-			"<td class='count'>" + items[k].count + "</td>" +
-			"<td class='inc'><button type='button' onclick='changeContent(1, " + items[k].id + ")'>+</button></td>" +
-			"<td class='count'>" + items[k].count * items[k].price + " SEK</td>" +
-			"<td class='del'><button type='button' onclick='changeContent(-Infinity, " + items[k].id + ")'>&#10799;</button></td>";
-		cart.appendChild(row);
-		sum += items[k].count * items[k].price
+			"<th class='beer-name'>" + cart[i].name + "</td>" +
+			"<td class='dec'><button type='button' onclick='changeQuantity(" + cart[i].id + ", -1)'>&minus;</button></td>" +
+			"<td class='count'>" + cart[i].quantity + "</td>" +
+			"<td class='inc'><button type='button' onclick='changeQuantity(" + cart[i].id + ", 1)'>+</button></td>" +
+			"<td class='count'>" + cart[i].quantity * cart[i].price + " SEK</td>" +
+			"<td class='del'><button type='button' onclick='removeItem(" + cart[i].id + ")'>&#10799;</button></td>";
+		tbody.appendChild(row);
+		sum += cart[i].quantity * cart[i].price;
 	}
 	document.getElementById("cart-sum").innerHTML = sum + "&nbsp;SEK";
 }
@@ -117,18 +173,17 @@ $(document).scroll(function() {
 });
 
 function initCart() {
-	beersPicked = sessionStorage.getItem("cart");
-	if (beersPicked != null) {
-		beersPicked = JSON.parse(beersPicked);
-	} else {
-		beersPicked = [];
-	}
-	redo = sessionStorage.getItem("redo");
-	if (redo != null) {
-		redo = JSON.parse(redo);
-	} else {
-		redo = [];
-	}
+	cart      = sessionStorage.getItem("cart");
+	cart      = cart      ? JSON.parse(cart)      : [];
+	undoStack = sessionStorage.getItem("undo");
+	undoStack = undoStack ? JSON.parse(undoStack) : [];
+	redoStack = sessionStorage.getItem("redo");
+	redoStack = redoStack ? JSON.parse(redoStack) : [];
+	document.getElementById("nav-undo").addEventListener("click", undo,      false);
+	document.getElementById("nav-redo").addEventListener("click", redo,      false);
+	document.getElementById("cancel"  ).addEventListener("click", clearCart, false);
+	document.getElementById("nav-undo").disabled = (undoStack.length == 0);
+	document.getElementById("nav-redo").disabled = (redoStack.length == 0);
 	updateCart();
 }
 
